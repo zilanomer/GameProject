3cd4117b112583d30bf191957e6e5f25e7b8bf24
Zilan Ömer <zilan.omer@setup34.com.tr>
2023-03-17 16:39:03 +0300

    validate simulation

--- GameProjectBackend/UserManager.cs
+++ GameProjectBackend/UserManager.cs
@@ -8,15 +8,30 @@ namespace GameProjectBackend
 {
     public class UserManager : IUserService
     {
-     
+        //UserValidation ı kullanabilmek için burda soyut bir const oluştururuz.
+        IUserValidation _userValidation;
+        public UserManager(IUserValidation userValidation)   //UserManager a bir bağımlılığı var UserManager içeriisnde bir doğrulama servisi kullanılacak.
+        {
+            _userValidation = userValidation;
+        }
+
         public void Add(User user)
         {
-            Console.WriteLine(user.FirstName+ " eklendi");
+            if (_userValidation.Validate(user))
+            {
+                Console.WriteLine(user.FirstName + " eklendi");
+            }
+
+            else
+            {
+                Console.WriteLine("Doğrulama Başarısız. Kayıt Başarısız.");
+            }
+            
         }
 
         public void Delete(User user)
         {
-            Console.WriteLine(user.FirstName+ " silindi");
+            Console.WriteLine(user.FirstName + " silindi");
         }
 
         public void Remove(User user)
---
+++ GameProjectBackend/UserValidateManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProjectBackend
+{
+    public class UserValidateManager : IUserValidation
+    {
+       
+        bool IUserValidation.Validate(User user)
+        {
+            if(user.FirstName == "Zilan")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
